056f60855a08927606eceaa9d9c3a53a9dd5d51a
tkyk.niimura <tkyk.niimura@gmail.com>
2019-12-21 01:53:06 +0900

    コードビハインド追加

---
+++ SimpleButton3/Pages/CodeBehind.razor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace SimpleButton3.Pages
+{
+    public partial class CodeBehind : ComponentBase
+    {
+        public string messageText { get; set; }
+        public double _top { get; set; } = 160;
+        public double _left { get; set; } = 20;
+
+        [Inject]
+        private NavigationManager NavMgr { get; set; }
+
+        public void DivClick(MouseEventArgs e)
+        {
+            _top = e.ClientY;
+            _left = e.ClientX;
+        }
+
+        public void MouseMove(MouseEventArgs e)
+        {
+            messageText = $"onMouseMove(x,y)=({e.ClientX},{e.ClientY})";
+        }
+
+        public void ClickReturn(MouseEventArgs e)
+        {
+            NavMgr.NavigateTo("/");
+        }
+    }
+}
