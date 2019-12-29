eef6bcd123c82690a523307e68f9ca866db11de9
tkyk.niimura <tkyk.niimura@gmail.com>
2019-12-29 23:30:55 +0900

    マスタメンテ追加

---
+++ TextBoxApp4/Data/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TextBoxApp4.Data
+{
+    /// <summary>
+    /// ユーザー
+    /// </summary>
+    //[Table("User", Schema = "dbo")]
+    public class User
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        /// <value></value>
+        public int Id { get; set; }
+        /// <summary>
+        /// Name of the user
+        /// </summary>
+        /// <value></value>
+        public string Name { get; set; }
+
+        public DateTime? Birth { get; set; }
+
+        [NotMapped]
+        public bool Insert { get; set; } = false;
+    }
+}
